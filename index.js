cb89cbb222423795e728d3a12ee5956f726c29db
Ryan Regalado <ryan@design48.net>
2017-01-10 14:34:22 -0800

    vars and env variable. added copy paste

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-var Guggy = require('@guggy/guggy-js-sdk')
-var opts = {
+#! /usr/bin/env node
+const args = process.argv
+const Guggy = require('@guggy/guggy-js-sdk')
+require('copy-paste').global()
+
+const opts = {
   apiKey: process.env.GUGGY_API_KEY
 }
-var sentence = 'barking dogs'
+
+var sentence = args[2] || 'barking dogs'
 
 Guggy.init(opts);
 
@@ -10,6 +15,9 @@ Guggy.createGug(sentence, function (result, err) {
   if (err) {
     console.log('there was an error', err)
   } else {
-    console.log('this is the result', result)
+    console.log('\n ', result)
+
+    copy(result.gif)
+    console.log('\n' + result.gif, 'has been copied to your clipboard')
   }
 })
